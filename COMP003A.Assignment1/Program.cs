075e8ff3b90c70ff7a01bd0b37a580cd1d60559e
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2023-01-18 20:35:35 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,76 @@
+﻿//this is a single line comment//
+/*this is a multi-line comment
+ * Author: Ruben Villagrana
+ * Course: Comp-003A-L01
+ * Code for assignment 1
+*/
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("100 things about me");
+            Console.WriteLine("I have a wonderful wife");   
+            Console.WriteLine("My name is Ruben");
+            Console.WriteLine("I am from Avenal Ca.");
+            Console.WriteLine("I am an Army Veteran");
+            Console.WriteLine("I joined the military when I was 18 years old");
+            Console.WriteLine("I was a parachutist for the 82nd Airborne Division");
+            Console.WriteLine("I was scared when I left home");
+            Console.WriteLine("I signed a 3 year enlistment");
+            Console.WriteLine("My recruter lied to me");
+            Console.WriteLine("Joining the military was a great move for me");
+            Console.WriteLine("I made life long friends");
+            Console.WriteLine("I used my GI bill and started going to college");
+            Console.WriteLine("I am a first generation Mexican");
+            Console.WriteLine("My upbringing was a little tough");
+            Console.WriteLine("I have 1 sister");
+            Console.WriteLine("I have 3 children");
+            Console.WriteLine("Roman is my oldest son");
+            Console.WriteLine("Roman is 19 years old");
+            Console.WriteLine("Joaquin is my middle child");
+            Console.WriteLine("Joaquin is 16 years old");
+            Console.WriteLine("Natalia is the baby");
+            Console.WriteLine("Natalia is 15 years old");
+            Console.WriteLine("Roman attends WestHills college");
+            Console.WriteLine("He will be graduating May 2023");
+            Console.WriteLine("Roman applied to Fresno state");
+            Console.WriteLine("Roman also applied to San Diego State");
+            Console.WriteLine("Roman is a sociology major");
+            Console.WriteLine("I am very proud of him");
+            Console.WriteLine("I am getting old");
+            Console.WriteLine("Joaquing goes to Lemoore High");
+            Console.WriteLine("He is a Junior in High shcool");
+            Console.WriteLine("Joaquin is in the engineering program");
+            Console.WriteLine("Joaqin loves number");
+            Console.WriteLine("Joaqin is a math nerd...lol");
+            Console.WriteLine("Joaquin wants to be an engineer");
+            Console.WriteLine("Joaquin likes working out");
+            Console.WriteLine("Joaqin loves watching football");
+            Console.WriteLine("Joaquin is a good student");
+            Console.WriteLine("He doesn't know exaclty what type of engineer though");
+            Console.WriteLine("Natalia is a sophmore");
+            Console.WriteLine("Natalia wants wants to be a dermatoligist");
+            Console.WriteLine("Natalia is also good in math");
+            Console.WriteLine("Kids are expensive");
+            Console.WriteLine("Teenagers have alot of attitude");
+            Console.WriteLine("I still love them though....lol");
+            Console.WriteLine("I have 3 dogs");
+            Console.WriteLine("Lola");
+            Console.WriteLine("Gracie");
+            Console.WriteLine("Ruby");
+            Console.WriteLine("Lola was a stray dog");
+            Console.WriteLine("She was out first dog");
+            Console.WriteLine("Gracie was given to us by a friend");
+            Console.WriteLine("Gracie is always happy go lucky");
+            Console.WriteLine("Gracie is a yorkie");
+            Console.WriteLine("Ruby was a shelter dog");
+            Console.WriteLine("We adopted her as a young pup");
+            Console.WriteLine("Ruby has a weird long neck");
+            Console.WriteLine("Ruby has found her forever home");
+              
+        }
+    }
+}
